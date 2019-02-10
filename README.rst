cadae7635f7588e8c7250e110c76f8d4de958e58
Subin <subin@apache.org>
2019-02-10 16:11:24 -0500

    add tf mnist

--- README.rst
+++ README.rst
@@ -6,29 +6,74 @@ pl-tensorflowapp
 Abstract
 ********
 
-test tf apps
+Sample Tensorflow application plugin for Chris Project.
+The application is a digit-identification application based on MNIST data.
 
 Run
 ***
 
+.. code-block:: bash
+
+usage: tensorflowapp.py [-h] [--json] [--savejson DIR] [--inputmeta INPUTMETA]
+                        [--saveinputmeta] [--saveoutputmeta] [--prefix PREFIX]
+                        [--inference_path imagedir] inputdir outputdir
+
+Runs the tensorflow application.
+
+positional arguments:
+  inputdir              directory containing the input files
+  outputdir             directory containing the output files/folders
+  inference_path        directory containing the test digit images
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --json                show json representation of app and exit
+  --savejson DIR        save json representation file to DIR and exit
+  --inputmeta INPUTMETA
+                        meta data file containing the arguments passed to this
+                        app
+  --saveinputmeta       save arguments to a JSON file
+  --saveoutputmeta      save output meta data to a JSON file
+  --prefix PREFIX       prefix for file names
+
+
+
 Using ``docker run``
 ====================
 
-Assign an "input" directory to ``/incoming`` and an output directory to ``/outgoing``
+Assign an "input" directory to ``/incoming`` and an output directory to ``/outgoing``.
+The input is prepopulated with mnist data.
 
 .. code-block:: bash
 
-    docker run --rm -v $(pwd)/in:/incoming -v $(pwd)/out:/outgoing   \
-            fnndsc/pl-tensorflowapp tensorflowapp.py            \
+    mkdir -p input && mkdir -p output
+
+
+
+
+**To train the mnist model**.
+Below command will train a mnist model and output the accuracy to a file in ``$(pwd)/output`` folder.
+
+.. code-block:: bash
+
+    docker run --rm -v $(pwd)/input:/incoming -v $(pwd)/output:/outgoing   \
+            submod/pl-tensorflowapp tensorflowapp.py            \
+            --prefix mnist-                                     \
             /incoming /outgoing
 
-This will ...
-
-Make sure that the host ``$(pwd)/out`` directory is world writable!
 
 
+**To train the mnist model & also run inference**.
+Below command will train a mnist model also run inference on the test image against the mnist model.
+
+.. code-block:: bash
+
+    docker run --rm -v $(pwd)/input:/incoming -v $(pwd)/output:/outgoing   \
+            submod/pl-tensorflowapp tensorflowapp.py            \
+            --prefix mnist-                                     \
+            --inference_path /incoming/test/test.png            \
+            /incoming /outgoing
 
 
 
-
-
+Make sure that the host ``$(pwd)/output`` directory is world writable!
--- tensorflowapp/tensorflowapp.py
+++ tensorflowapp/tensorflowapp.py
@@ -9,6 +9,10 @@
 #
 
 import os
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from tensorflow.examples.tutorials.mnist import input_data
 
 # import the Chris app superclass
 from chrisapp.base import ChrisApp
@@ -25,7 +29,7 @@ class Tensorflowapp(ChrisApp):
     TITLE           = 'tf example'
     CATEGORY        = 'tensorflow'
     TYPE            = 'ds'
-    DESCRIPTION     = 'test tf apps'
+    DESCRIPTION     = 'Sample Tensorflow application plugin for Chris Project.'
     DOCUMENTATION   = 'http://wiki'
     VERSION         = '0.1'
     ICON            = '' # url of an icon image
@@ -48,12 +52,79 @@ class Tensorflowapp(ChrisApp):
         """
         Define the CLI arguments accepted by this plugin app.
         """
+        self.add_argument('--prefix', dest='prefix', type=str, optional=False,
+                          help='prefix for file names')
+        self.add_argument('--inference_path', dest='inference_path', type=str, optional=False,
+                          help='path of images')
 
     def run(self, options):
         """
         Define the code to be run by this plugin app.
         """
+        self.run_tensorflow_app(options)
 
+    def run_tensorflow_app(self, options):
+        digit_image = None
+        if options.inference_path:
+            str_path = os.path.abspath(options.inference_path)
+            infer_image = Image.open(str_path)
+            np_image = np.array(infer_image)
+            np_image = np_image.flatten() / 255.0
+            digit_image = np.reshape(np_image, (1, 784))
+            print("Test Image shape: ", digit_image.shape)
+        self.mnist_training(options, digit_image)
+
+    def mnist_training(self, options, digit_image):
+        mnist = input_data.read_data_sets(options.inputdir + "/data", one_hot=True)
+        image_size = 28
+        labels_size = 10
+        learning_rate = 0.05
+        steps_number = 1000
+        batch_size = 100
+        # x is training data
+        x = tf.placeholder(tf.float32, [None, image_size * image_size])
+        labels = tf.placeholder(tf.float32, [None, labels_size])
+        W = tf.Variable(tf.truncated_normal([image_size * image_size, labels_size], stddev=0.1))
+        b = tf.Variable(tf.constant(0.1, shape=[labels_size]))
+        # y is the output
+        y = tf.matmul(x, W) + b
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=y))
+        training_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+        prediction = tf.equal(tf.argmax(y, 1), tf.argmax(labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+        sess = tf.InteractiveSession()
+        sess.run(tf.global_variables_initializer())
+        for i in range(steps_number):
+            input_batch, labels_batch = mnist.train.next_batch(batch_size)
+            feed_dict = {x: input_batch, labels: labels_batch}
+            training_step.run(feed_dict=feed_dict)
+            if i % 100 == 0:
+                train_accuracy = accuracy.eval(feed_dict=feed_dict)
+                print("Step %d, training batch accuracy %g %%" % (i, train_accuracy * 100))
+        test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images, labels: mnist.test.labels})
+        acc = test_accuracy * 100
+        print("Test accuracy: ", acc)
+        self.create_output(options, "accuracy", acc)
+
+        if digit_image is not None:
+            prediction = sess.run(y, feed_dict={x: digit_image}).argmax()
+            print("Inference value of test Image is : ", prediction)
+            self.create_output(options, "inference", prediction)
+
+    def create_output(self, options, key, value):
+        new_name = options.prefix + key
+        str_outpath = os.path.join(options.outputdir, new_name)
+        str_outpath = os.path.abspath(str_outpath)
+        print('Creating new file... %s' % str_outpath)
+        if not os.path.exists(options.outputdir):
+            try:
+                os.mkdir(options.outputdir)
+            except OSError:
+                print("Creation of the directory %s failed" % options.outputdir)
+            else:
+                print("Successfully created the directory %s " % options.outputdir)
+        with open(str_outpath, 'w') as f:
+            f.write(str(value))
 
 
 # ENTRYPOINT
